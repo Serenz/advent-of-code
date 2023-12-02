f340ff8d35149abf3d8a4b593f4e9030210ae6e5
Marco <marcoserena96@gmail.com>
2023-12-02 23:06:24 +0100

    day2 part 2

--- day2/day2_part2.py
+++ day2/day2_part2.py
@@ -6,12 +6,6 @@
     https://adventofcode.com/2023/day/2#part2
 """
 
-OK = {
-    'red': 12,
-    'green': 13,
-    'blue': 14
-}
-
 
 if __name__ == '__main__':
     with open('day2_input.txt', 'r') as f:
@@ -21,15 +15,14 @@ if __name__ == '__main__':
         num, hands = game.replace('\n', '').split(': ')
         num = int(num.split(' ')[1])
         hands = hands.split('; ')
-        flag = True
+        mins = {}
         for hand in hands:
             picks = hand.split(', ')
-            if flag:
-                for pick in picks:
-                    val, color = pick.split(' ')
-                    if int(val) > OK[color]:
-                        flag = False
-                        break
-        if flag:
-            result += num
+            for pick in picks:
+                val, color = pick.split(' ')
+                mins[color] = max(mins.get(color, int(val)), int(val))
+        t = 1
+        for v in mins.values():
+            t *= v
+        result += t
     print(result)
